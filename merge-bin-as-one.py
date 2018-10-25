3784d86c6ded3c4a753cd07810fc94a6b8bf736b
Yonggang Liu <Yonggang.G.Liu@ericsson.com>
2018-10-25 09:37:50 +0800

    Modified the shape of the progress bar

--- merge-bin-as-one.py
+++ merge-bin-as-one.py
@@ -13,7 +13,7 @@ bin_map_info = []
 #Parsing config file, and get the file name\length and its start address in flash.
 with open('address-map.ini') as config_file:
     try:
-        for line in config_file.readlines():
+        for line in tqdm(config_file.readlines(), desc="Parsing config"):
             if not line.lstrip().startswith('#'):
                 bin_map_info.append([line.split(',')[0].strip(), line.split(',')[1].strip(), line.split(',')[2].strip()])
     finally:
@@ -23,7 +23,7 @@ with open('address-map.ini') as config_file:
 
 #Creating a file which is filled by binary 1 and its size is the same with flash of DOT
 with open('One-Entire-Image.bin', 'wb') as one_image:
-    for i in trange(0xffffff):
+    for i in trange(0xffffff, desc="Generating bin"):
         one_image.write(pack('B', 255))
     one_image.close()
 
@@ -31,13 +31,12 @@ with open('One-Entire-Image.bin', 'wb') as one_image:
 #Second step:
 #According to these infomations to get the related file and read out it as binary format.
 #Then write its into the file which is deemed as the flash. Do it one by one.
-pbar = tqdm(bin_map_info)
-for item_bin in pbar:
+for item_bin in tqdm(bin_map_info, desc="Merging    bin"):
     image_name, image_addr, image_len = item_bin
-    pbar.set_description("Processing %s" % image_name)
+    #pbar.set_description("Processing %s" % image_name)
     #print(image_name, image_addr, image_len)
     if not os.path.exists('full-loads/'+image_name):
-        print("Warning: %s isn't exist!!!!" % ('full-loads/'+image_name))
+        print("\nFailure, because: %s isn't exist!!!!" % ('full-loads/'+image_name))
         break
     
     file_size = os.path.getsize('full-loads/'+image_name)
